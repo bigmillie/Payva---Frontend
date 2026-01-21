1165c6e617e3c2ea42bb4420a42b48a23e6f7413
Okiemute Egokiphovwen <ego.okiemute@gmail.com>
2026-01-21 10:42:51 +0100

    feat: send email done

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -21,8 +21,8 @@ export async function sendWelcomeEmail({ to, firstName }: EmailOptions) {
 
   // HTML content based on your template
   const html = `
-    <div style="font-family: sans-serif; line-height: 1.5; color: #1F2937; padding: 20px;">
-      <img src="https://res.cloudinary.com/kodenigga/image/upload/v1766153647/icons-for%20email-templates/bxqju1mxghzve01l75lc.svg" alt="Payva Logo" style="width: 120px; padding-bottom: 30px;"/>
+    <div style="font-family: sans-serif; line-height: 1.5; color: #1F2937; padding: 20px; max-width: 600px; margin: auto;">
+      <img src="https://res.cloudinary.com/kodenigga/image/upload/v1768988493/icons-for%20email-templates/avblwtab8ml7hjba6kgb.png" alt="Payva Logo" style="width: 120px; padding-bottom: 30px;"/>
       <img src="https://res.cloudinary.com/kodenigga/image/upload/v1768987991/icons-for%20email-templates/x0hxi3fz2ogje51xpwyn.png" alt="Welcome Illustration" style="width: 100%; max-width: 400px; padding-bottom: 30px;"/>
       <h2 style="color: #006D68;">${greeting}</h2>
       <p>We're so glad you're here.</p>
@@ -30,19 +30,16 @@ export async function sendWelcomeEmail({ to, firstName }: EmailOptions) {
         By joining our waitlist, you've just secured your spot to be among the first to experience Payva — 
         a smarter, simpler way to handle your money, no matter where life takes you.
       </p>
-      <br />
       <p>
         We know managing finances across borders can feel complicated and disconnected. 
         We're changing that. Payva is being built to make your money work as seamlessly as you do — 
         whether you're sending funds home, paying bills across countries, or just keeping everything organised in one place.
       </p>
-      <br />
       <p>
         Here's what happens next:<br/>
         You'll get early access when we launch, plus exclusive updates and perks along the way. 
         Think sneak peeks at new features, special offers, and maybe a few surprises we're keeping under wraps for now.
       </p>
-      <br />
       <p>We're building something we think you'll love, and we're excited to have you with us from the start.</p>
       <p>Stay tuned — good things are coming.</p>
       <br/>
