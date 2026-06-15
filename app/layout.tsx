e9a30a7a28b8eebaf4f4f33eff039982ee7d9880
Okiemute Egokiphovwen <ego.okiemute@gmail.com>
2026-06-15 12:36:24 +0100

    meta pixels tag added

--- app/layout.tsx
+++ app/layout.tsx
@@ -140,6 +140,22 @@ export default function RootLayout({
             })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
           `}
         </Script>
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1454547299689771');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className="antialiased">
         {/* Google Tag Manager (noscript) */}
@@ -149,6 +165,18 @@ export default function RootLayout({
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {/* Meta Pixel (noscript) */}
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1454547299689771&ev=PageView&noscript=1"
+            alt=""
           />
         </noscript>
         <CurrencyProvider>{children}</CurrencyProvider>
